9dcc4035559edfaade9a7126426570592c094855
Sean Nicholson <snicholson@cloudpassage.com>
2016-06-03 11:33:04 -0400

    V1.1
    Added additional error checking for group ID and group names
    Added loop for put statement to move group with a retry count of 3

--- move_deactivated_servers.py
+++ move_deactivated_servers.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python
-
-
-##########################################################################################################
+###############################################################################
 # Import statements
-# This script requires the following Pyton modules
+# This script requires the following Python modules
 # json, base64, datetime, sys, argparse
-# use PIP import for these if you are encoutering errors for pytz, requests, iso8601
+# use PIP import for these if you are encountering errors for pytz, requests,
+# iso8601
 #   > pip install requests, pytz, iso8601
 #
 
 import json, base64, requests, datetime, sys, argparse, pytz, iso8601
 from config import clientID, clientSecret, apiurl, deactivate_num_days, moveToGroupName, moveToGroupID
 
-##########################################################################################################
+###############################################################################
 
 
-##########################################################################################################
+###############################################################################
 # Variables
-# Please edit these values in the config.auth.  You can find these information from HALO "[Site Administration] -> [API Keys]" page
+# Please edit these values in the config.auth.  You can find these information
+# from HALO "[Site Administration] -> [API Keys]" page
+
 if len(clientID) == 8 and len(clientSecret) == 32:
     api_key_id = clientID
     api_secret_key = clientSecret
@@ -33,13 +33,15 @@ if not (moveToGroupName or moveToGroupID):
     print "Please configure destination group in config.py"
     print "Exiting..."
     sys.exit(1)
-
+###############################################################################
 # Other variables
 client_credential = api_key_id + ":" + api_secret_key
 user_credential_b64 = "Basic " + base64.b64encode(client_credential)
 
-##########################################################################################################
+###############################################################################
 
+###############################################################################
+# Define Methods
 
 def get_headers():
     # Create headers
@@ -56,41 +58,90 @@ def get_access_token(url, query_string, headers):
 def move_group(host_id,group_id):
     data = { "server": {"group_id": group_id}}
     status_code = str("404")
+    retry_loop_counter = 0
     moveurl = apiurl + "/v1/servers/" + host_id
     #print ("URL: %s") % moveurl
     #print ("Request Body: %s" % data)
-    reply = requests.put( moveurl, data=json.dumps(data), headers=headers)
-    status_code = str(reply.status_code)
-    print ("Result of group move: %s" % status_code)
-    return True
 
+    # Loop to attempt server move PUT request
+    # will retry 4 times to move server if status_code not 204
+    while (retry_loop_counter < 3):
+        reply = requests.put( moveurl, data=json.dumps(data), headers=headers)
+        status_code = str(reply.status_code)
+        #print ("Result of group move: %s" % status_code)
+        retry_loop_counter += 1
+        if status_code == "204":
+            # Arbitrary number to exit loop
+            retry_loop_counter = 5
+            return True
+        else:
+            print "Failed to move server...Retry attempt %d" % retry_loop_counter
+
+
+# Get groupID from group name specified
 def get_group_id(groupName):
+    groupID_found = 0
     groupurl = api_request_url + "/v1/groups"
     reply = requests.request("GET", groupurl, data=None, headers=headers)
     for group in reply.json()["groups"]:
         if group['name'] == groupName:
-            return group['id']
+            newgroupID = group['id']
+            groupID_found += 1
+    # Sanity checks for group name matched
+    if groupID_found == 1:
+        return newgroupID
+    elif groupID_found > 1:
+        print "More than 1 group matched group name"
+        print "Please specify unique group name"
+        print "Exiting..."
+        sys.exit(1)
+    else:
+        print "Group name match not found"
+        print "Please check group name or API Key scope"
+        print "Exiting..."
+        sys.exit(1)
 
+# check for defined groupID to move servers to
+# if not groupID specified in config file, then
+# run get_group_id
+def check_group_id():
+    if (moveToGroupID):
+        newgroupID = str(moveToGroupID)
+        #print "group_id set to %s" % newgroupID
+        return newgroupID
+    else:
+        newgroupID = str(get_group_id(moveToGroupName))
+        #print "Moving to group %s with group_id: %s" % moveToGroupName, newgroupID
+        #print moveToGroupName, newgroupID
+        return newgroupID
 
-def move_deactivated_servers():
+def get_deactivated_server_list():
     deactivatedServersURL = api_request_url + "/v1/servers?state=deactivated"
     reply = requests.request("GET", deactivatedServersURL, data=None, headers=headers)
+    return reply
+
+def move_deactivated_servers():
+    global newgroupID
     servers_moved = 0
     servers_ignored = 0
     servers_previously_moved = 0
+
+    # Check for defined groupID to move servers to
+    #print "Move to Group ID: %s" % moveToGroupID
+    newgroupID = check_group_id()
+
+    # How many days should a server be offline before being moved?
+    deactivate_days = deactivate_num_days
+
+    # Get list of deactivated servers
+    reply = get_deactivated_server_list()
+
+    # Loop through deactivated servers list
+    # and move if move criteria met
     for server in reply.json()["servers"]:
-        if (moveToGroupID):
-            newgroupID = str(moveToGroupID)
-            #print "group_id set to %s" % newgroupID
-        else:
-            newgroupID = str(get_group_id(moveToGroupName))
-            #print "Moving to group %s with group_id: %s" % moveToGroupName, newgroupID
-            #print moveToGroupName, newgroupID
         server_id = server['id']
         server_hostname = server['hostname']
-        # How many days should a server be offline before being retired?
-        #retire_days = -1
-        deactivate_days = deactivate_num_days
+
         # Create aware datetime object for last time seen
         lastseen = iso8601.parse_date(server['last_state_change'])
 
@@ -109,11 +160,12 @@ def move_deactivated_servers():
             print "Server %s already moved -- ignoring." % server_hostname
             servers_previously_moved += 1
 
-        # If server older than retire_days days, move to retired
+        # If server older than deactivate_days days, move to newgroupID
         elif (diff_days > deactivate_days and server_id):
             #print server_id
             #print server_hostname
             #print newgroupID
+            # Move Server to Deactivated group
             data  = move_group(server['id'],newgroupID)
             if data:
                 print "Server %s moved successfully." % server_hostname
@@ -125,13 +177,18 @@ def move_deactivated_servers():
                 elif diff_days <= deactivate_days:
                     print "Server %s has been offline for %s days.\n" % (server_hostname, diff_days)
                     servers_ignored += 1
+
+    #Print summary of script actions
     print "\n********Script Summary********"
     print "Servers moved: %d" % servers_moved
     print "Servers ignored, less than specified days deactivated: %d " % servers_ignored
     print "Servers already in deactivated group: %d" % servers_previously_moved
-    current_date_time = datetime.datetime.now()
-    print "Script completed: %s" % current_date_time
+    current_date_time = datetime.datetime.utcnow()
+    print "Script completed: %s UTC" % current_date_time
     print "********************************"
+###############################################################################
+
+
 #---MAIN---------------------------------------------------------------------
 
 
